710d21173f2e141bb5f0110278ad7495b860b375
JanniHJK <janni.enghave.andersen@hjoerring.dk>
2026-07-14 11:32:25 +0200

    Add lungMission object with KOL-related questions

---
+++ missions/lunger.js
@@ -0,0 +1,120 @@
+const lungMission = {
+
+title:"Lungernes hemmelighed",
+
+steps:[
+
+
+{
+text:
+"Erik fortæller, at han bliver mere forpustet. Hvad er den vigtigste årsag ved KOL?",
+
+answers:[
+
+"Det bliver sværere at få luften ud af lungerne",
+
+"Lungerne stopper helt med at arbejde",
+
+"Kroppen producerer for lidt blod"
+
+],
+
+correct:0,
+
+feedback:
+"Ved KOL bliver luftvejene ofte forsnævrede, og lungevævet kan være påvirket. Det gør det svært at få luften ud."
+},
+
+
+
+{
+text:
+"Hvilket symptom er typisk ved KOL?",
+
+answers:[
+
+"Åndenød ved aktivitet",
+
+"Pludselig høretab",
+
+"Brækkede knogler"
+
+],
+
+correct:0,
+
+feedback:
+"Åndenød er et af de mest almindelige symptomer ved KOL."
+},
+
+
+
+{
+text:
+"Erik fortæller, at han ofte hoster slim op. Hvad kan det være tegn på?",
+
+answers:[
+
+"Kronisk irritation i luftvejene",
+
+"At lungerne er blevet større",
+
+"At han mangler væske i kroppen"
+
+],
+
+correct:0,
+
+feedback:
+"Ved kronisk bronkitis kan slimproduktionen være øget."
+},
+
+
+
+{
+text:
+"Hvad kan hjælpe en borger med KOL og åndenød?",
+
+answers:[
+
+"Rolig vejrtrækning og pauser",
+
+"At undgå al bevægelse",
+
+"At holde vejret så længe som muligt"
+
+],
+
+correct:0,
+
+feedback:
+"Teknikker til vejrtrækning og energibesparelse kan hjælpe."
+},
+
+
+
+{
+text:
+"Hvad er vigtigt som medarbejder?",
+
+answers:[
+
+"Observere ændringer og reagere tidligt",
+
+"Kun fokusere på medicinen",
+
+"Vente til borgeren selv siger til"
+
+],
+
+correct:0,
+
+feedback:
+"Tidlig opsporing er vigtig ved KOL."
+
+}
+
+
+]
+
+};

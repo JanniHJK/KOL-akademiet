e0fc01466eaaa7b45bdce8f500be89854449b57e
JanniHJK <janni.enghave.andersen@hjoerring.dk>
2026-07-14 10:42:20 +0200

    Add createProfile function to handle player creation

--- js/profile.js
+++ js/profile.js
@@ -1,1 +1,35 @@
+function createProfile(){
 
+const name =
+document.getElementById("player-name").value;
+
+
+if(!name){
+alert("Skriv dit navn først");
+return;
+}
+
+
+const player = {
+
+name:name,
+
+rank:"Ny medarbejder",
+
+xp:0,
+
+progress:0,
+
+badges:[]
+
+};
+
+
+savePlayer(player);
+
+
+showScreen("academy-screen");
+
+updateDashboard();
+
+}

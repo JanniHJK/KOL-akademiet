18f64f56fb83d2d5492f311a2c85098676ab4dba
JanniHJK <janni.enghave.andersen@hjoerring.dk>
2026-07-14 10:41:58 +0200

    Add functions to save and retrieve player data

--- js/storage.js
+++ js/storage.js
@@ -1,1 +1,17 @@
+function savePlayer(player){
 
+localStorage.setItem(
+"KOL_player",
+JSON.stringify(player)
+);
+
+}
+
+
+function getPlayer(){
+
+return JSON.parse(
+localStorage.getItem("KOL_player")
+);
+
+}
